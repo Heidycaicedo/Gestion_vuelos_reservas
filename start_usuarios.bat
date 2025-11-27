c1a5d17b55d2dd3e3ff6fb86830d84922def3833
Heidycaicedo <hycaicedo@uniboyaca.edu.co>
2025-11-27 17:30:59 -0500

    se borraron algunas carpetas que no estaban haciendo niguna funcionalidad

--- start_usuarios.bat
+++
@@ -1,6 +0,0 @@
-# Microservicio Usuarios
-# Puerto: 8001
-# URL base: http://localhost:8001
-
-cd c:\xampp\htdocs\Gestion_vuelos_reservas\microservicio_usuarios
-php -S localhost:8001
--- start_vuelos.bat
+++
@@ -1,6 +0,0 @@
-# Microservicio Vuelos
-# Puerto: 8002
-# URL base: http://localhost:8002
-
-cd c:\xampp\htdocs\Gestion_vuelos_reservas\microservicio_vuelos
-php -S localhost:8002
